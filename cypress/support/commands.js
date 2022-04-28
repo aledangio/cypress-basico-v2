d604dab35d979025c2dd552f409cff75fcc91411
Alexandre Dangio <aledangio@gmail.com>
2022-04-28 18:35:03 -0300

    Create cypress project

---
+++ cypress/support/commands.js
@@ -0,0 +1,7 @@
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function () {
+    cy.get('#firstName').type('Alexandre');
+    cy.get('#lastName').type('Dangio');
+    cy.get('#email').type('aledangio@gmail.com');
+    cy.get('#open-text-area').type('Teste', { delay: 0 });
+    cy.get('.button').click();
+})
